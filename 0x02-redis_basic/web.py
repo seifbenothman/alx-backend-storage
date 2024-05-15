279b2518565dd6afd7c8e8df21ba6e87da2eb40f
seifbenothman <seifbenothman95@gmail.com>
2024-05-15 14:36:40 +0000

    fix

--- 0x02-redis_basic/web.py
+++ 0x02-redis_basic/web.py
@@ -1,24 +1,26 @@
 #!/usr/bin/env python3
-""" web """
+"""web.py"""
 import requests
 import redis
 from functools import wraps
 
+# Initialize Redis connection
+redis_conn = redis.Redis()
 
 def track_and_cache(func):
-    """ track / cache """
+    """Decorator to track and cache web page content."""
     @wraps(func)
     def wrapper(url):
-        """ rap god """
-        redis_conn = redis.Redis()
-
+        """Wrapper function for tracking and caching."""
         url_count_key = f"count:{url}"
         redis_conn.incr(url_count_key)
 
         cached_content = redis_conn.get(url)
         if cached_content:
+            print("Cache hit!")
             return cached_content.decode('utf-8')
 
+        print("Cache miss!")
         response = requests.get(url)
         page_content = response.text
 
@@ -28,15 +30,16 @@ def track_and_cache(func):
 
     return wrapper
 
-
 @track_and_cache
 def get_page(url: str) -> str:
-    """ page """
-    return requests.get(url).text
-
+    """Fetch the page content of a URL."""
+    response = requests.get(url)
+    return response.text
 
 if __name__ == "__main__":
     url = ("http://slowwly.robertomurray.co.uk/"
            "delay/1000/url/"
            "https://www.example.com")
+    print(get_page(url))
+    print(get_page(url))
     print(get_page(url))
