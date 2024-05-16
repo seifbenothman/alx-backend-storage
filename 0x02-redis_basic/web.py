17163b55971e05feec8de34718e1bd7937e71220
seifbenothman <seifbenothman95@gmail.com>
2024-05-16 00:59:43 +0000

    web

--- 0x02-redis_basic/web.py
+++ 0x02-redis_basic/web.py
@@ -1,26 +1,25 @@
 #!/usr/bin/env python3
-""" web.py """
+""" Module for web functions """
+
 import requests
 import redis
 from functools import wraps
-import time
 
-redis_conn = redis.Redis(host='localhost', port=6379, db=0)
 
 def track_and_cache(func):
-    """Decorator to track and cache web page content."""
+    """ Decorator to track and cache page requests """
     @wraps(func)
     def wrapper(url):
-        """Wrapper function for tracking and caching."""
+        """ Wrapper function to track and cache page requests """
+        redis_conn = redis.Redis()
+
         url_count_key = f"count:{url}"
         redis_conn.incr(url_count_key)
 
         cached_content = redis_conn.get(url)
         if cached_content:
-            print("Cache hit!")
             return cached_content.decode('utf-8')
 
-        print("Cache miss!")
         response = requests.get(url)
         page_content = response.text
 
@@ -30,24 +29,15 @@ def track_and_cache(func):
 
     return wrapper
 
+
 @track_and_cache
 def get_page(url: str) -> str:
-    """Fetch the page content of a URL."""
+    """ Function to get page content """
     return requests.get(url).text
+
 
 if __name__ == "__main__":
     url = ("http://slowwly.robertomurray.co.uk/"
            "delay/1000/url/"
            "https://www.example.com")
     print(get_page(url))
-    print(get_page(url))
-    print(get_page(url))
-
-    time.sleep(11)
-    print(get_page(url))
-
-    access_count = redis_conn.get(f"count:{url}")
-    if access_count:
-        print(f"URL was accessed {access_count.decode('utf-8')} times.")
-    else:
-        print("URL access count not found.")
